ba99e21261c4ce3331f770c17e69f85899a5d7ef
Dinesh <Dineshhire4@gmail.com>
2022-02-11 17:13:02 +0530

    Creating First Componant (Top Headline)

---
+++ src/app/topheadline/topheadline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topheadline',
+  templateUrl: './topheadline.component.html',
+  styleUrls: ['./topheadline.component.css']
+})
+export class TopheadlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
